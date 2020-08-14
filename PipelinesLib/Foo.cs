885d192138df722be0f7ba77cd010ae091f48e5c
Stephen Marshall <stephen@Finappshorsham.local>
2020-08-14 12:44:50 +0100

    Initial commit

---
+++ PipelinesLib/Foo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipelinesLib
+{
+    public class Foo
+    {
+        public string SayFoo()
+        {
+            return "foo";
+        }
+    }
+}
---
+++ PipelinesWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PipelinesWeb.MvcApplication" Language="C#" %>
